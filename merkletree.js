cb1498e1b30eba54bba9c86b53650948e680c0f1
Chen-Hsien <zxcvb1088@gmail.com>
2022-10-21 23:00:47 +0800

    Update merkletree.js

--- merkletree.js
+++ merkletree.js
@@ -10,12 +10,27 @@ let whitelistAddresses = [
 "0xa778C17aC550dF2FAf08d8646E52ab2e382eBF9d",
 "0xf7Ecd1D3E56f97e3caDA93dDA0AE6C8b3aDa27E0",
 "0xfac9d58FB9e80Cd36e86e10B63461f9c004287aF"
-]  
+]
 
 const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
-const merkleTree = new MerkleTree(leafNodes, keccak256);
+const merkleTree = new MerkleTree(leafNodes, keccak256, {sortPairs:true});
 
-const rootHash = merkleTree.getRoot();
+const rootHash = merkleTree.getHexRoot();
 
-console.log(rootHash);
-console.log("Merkle Tree Diagam\n", merkleTree.toString());
+ console.log(rootHash);
+// console.log("Merkle Tree Diagam\n", merkleTree.toString());
+
+let leaf = keccak256('0x7d566a978b786175B70F1E34a94560f5497E49CF');
+let proof = merkleTree.getHexProof(leaf);
+console.log('Proof of 0x7d566a978b786175B70F1E34a94560f5497E49CF: ', proof);
+
+let leaffake = keccak256('0x7d566a978b786175B70F1E34a94560f5497E49XX');
+proof = merkleTree.getHexProof(leaffake);
+console.log('Proof of 0x7d566a978b786175B70F1E34a94560f5497E49XX: ', proof);
+
+ const claimingAddress = leafNodes[0];
+ const hexProof = merkleTree.getHexProof(claimingAddress);
+ 
+ console.log(hexProof);
+ console.log(merkleTree.verify(hexProof, claimingAddress, rootHash))
+
